3ffd6019d26ccdd15fcc2efe4f33fb4b55bd89f4
Jaro <jaro@Jaros-HackBook-Pro.local>
2020-05-05 14:23:32 +0200

    various changes

---
+++ main.js
@@ -0,0 +1,48 @@
+const inputElement = document.getElementById("input");
+const patchButton = document.getElementById("patchBtn");
+const downloadBtn = document.getElementById("downloadBtn");
+const status = document.getElementById("status");
+const progBar = document.getElementById("progBar");
+var file;
+
+// status init
+patchButton.disabled = true;
+downloadBtn.style.display = "none";
+inputElement.addEventListener("change", handleFiles, false);
+
+function handleFiles() {
+    downloadBtn.style.display = "none";
+    status.innerHTML = "Ready"
+    file = this.files[0];
+    if (file.type == "video/mp4") {
+        patchButton.disabled = false;
+    } else {
+        patchButton.disabled = true;
+        console.log("Invalid file format");
+        alert("Invalid file format")
+    }
+}
+
+function patch() {
+    console.log("Starting patching process");
+    patchButton.disabled = true;
+
+    var angle = document.getElementById("angle").value * Math.PI / 180;
+    var patcher = new Worker('patcher.js');
+    patcher.postMessage([file, angle]);
+
+    patcher.onmessage = function (e) {
+        if (e.data instanceof Blob) {
+            downloadBtn.href = URL.createObjectURL(e.data);
+            downloadBtn.download = file.name.slice(0, -4) + "_patched.MP4";
+            downloadBtn.style.display = "inline";
+            progBar.style.width = "100%";
+            status.innerHTML = "Finished";
+            console.log("Successfully generated download link");
+        } else {
+            // update progress
+            status.innerHTML = e.data[0] + file.name;
+            progBar.style.width = e.data[1] + "%";
+        }
+    }
+}
---
+++ patcher.js
@@ -0,0 +1,78 @@
+var chunkSize = 1024 * 1024 * 1024; // 1 GB
+var chunks = [];
+var reader = new FileReader();
+var file;
+var angle;
+
+onmessage = function (e) {
+    file = e.data[0];
+    angle = e.data[1];
+    readChunk(0);
+}
+
+function patch() {
+    // loop over chunks
+    for (var k = 0; k < chunks.length; k++) {
+        var chunk = chunks[k];
+        // loop over batches of a chunk
+        for (var i = 0; i < chunk.length - 4; i++) {
+            if (chunk[i] == 0x41 && chunk[i + 1] == 0x43 && chunk[i + 2] == 0x43 && chunk[i + 3] == 0x4c && chunk[i + 4] == 0x73) {
+                // update progress
+                postMessage(["Patching ", (i + (k * chunkSize)) / file.size * 100]);
+                var sampleCount = chunk[i + 6] << 8 | chunk[i + 7]
+                // loop over samples of a batch
+                for (var j = 0; j < sampleCount; j++) {
+                    var index = i + j * 6 + 8
+                    // read original values
+                    var x = toInt16Bytes(chunk[index], chunk[index + 1]); // front/back
+                    var y = toInt16Bytes(chunk[index + 2], chunk[index + 3]); // left/right
+                    var z = toInt16Bytes(chunk[index + 4], chunk[index + 5]); // down/up
+                    // calculate new values
+                    var xNew = Math.round(x * Math.sin(angle) + y * Math.cos(angle));
+                    var yNew = z; // correct
+                    var zNew = Math.round(x * Math.cos(angle) - y * Math.sin(angle));
+                    // save the new values
+                    chunk[index] = xNew >> 8;
+                    chunk[index + 1] = xNew;
+                    chunk[index + 2] = yNew >> 8;
+                    chunk[index + 3] = yNew;
+                    chunk[index + 4] = zNew >> 8;
+                    chunk[index + 5] = zNew;
+                }
+            }
+        }
+    }
+    console.log("Successfully patched");
+    var output = new Blob(chunks, { type: 'video / mp4' });
+    postMessage(output);
+}
+
+// convert two chunk to a big-endian signed integer
+function toInt16Bytes(byteA, byteB) {
+    var sign = byteA & (1 << 7);
+    var result = (((byteA & 0xFF) << 8) | (byteB & 0xFF));
+    if (sign) {
+        result = 0xFFFF0000 | result;
+    }
+    return result;
+}
+
+function readChunk(start) {
+    var end = start + chunkSize;
+    var chunk = file.slice(start, end);
+    reader.onload = function (event) {
+        // add new chunk to the chunks array
+        chunks.push(new Uint8Array(event.target.result));
+        postMessage(["Loading ", end / file.size * 100]);
+        if (end < file.size) { // still data left
+            // update progress and load next chun
+            console.log("Successfully loaded chunk");
+            readChunk(end);
+        } else { // upload complete
+            // logging and status update
+            console.log("Successfully loaded file");
+            patch();
+        }
+    }
+    reader.readAsArrayBuffer(chunk);
+}
